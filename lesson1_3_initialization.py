ba8eb33a1ba196299ace16fd3e250bc8b20cffce
Asya Isaeva <asya.gush@gmail.com>
2018-10-29 10:41:58 +0100

    write testcase "success login" in pytest

---
+++ lesson1_3_initialization.py
@@ -0,0 +1,27 @@
+import os
+import pytest
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def create_chrome_driver():
+    home_dir = os.getenv('HOME')
+    driver_location = os.path.join(home_dir, 'bin', 'chromedriver')
+    return webdriver.Chrome(driver_location)
+
+
+@pytest.fixture
+def driver(request):
+    chrome = create_chrome_driver()
+    request.addfinalizer(chrome.quit)
+    return chrome
+
+
+def test_successful_login(driver: WebDriver):
+    driver.get("http://localhost:8080/litecart/admin/login.php")
+    driver.find_element_by_name("username").send_keys('admin', Keys.ENTER)
+    driver.find_element_by_name("password").send_keys('admin', Keys.ENTER)
+    WebDriverWait(driver, 10).until(EC.url_to_be("http://localhost:8080/litecart/admin/"))
