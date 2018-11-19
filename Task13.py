43e267c198444d92dfc994c17745ebc39cba187b
Asya Isaeva <asya.gush@gmail.com>
2018-11-19 12:50:07 +0100

    testcase add item to cart and remove

--- Task13.py
+++ Task13.py
@@ -1,13 +1,13 @@
 import pytest
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from drivers import drivers
 from steps import login_as_customer
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import time
+
 
 
 @pytest.fixture
@@ -23,29 +23,51 @@ def test_add_and_dell_item_to_cart(driver: WebDriver):
         pre_quantity = element.get_attribute("textContent")
         driver.find_elements_by_css_selector("#box-most-popular a")[0].click()
         WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.NAME, 'add_cart_product')))
-        time.sleep(1)
-        if is_select_present('select'):
+
+        if is_element_present('select'):
             select = Select(driver.find_element_by_css_selector('select'))
             select.select_by_visible_text('Small')
 
         driver.find_element_by_css_selector('button[name = "add_cart_product"]').click()
-        alert = WebDriverWait(driver, 5).until(EC.alert_is_present())
-        alert.accept()
+        is_alert_present()
         driver.refresh()
-        WebDriverWait(driver, 5).until(EC.staleness_of(element))
+        #        WebDriverWait(driver, 5).until(EC.staleness_of(element))
         assert pre_quantity != driver.find_element_by_css_selector("#cart-wrapper .quantity").get_attribute(
             "textContent")
         driver.back()
 
-    def is_select_present(selector):
+    def del_item_from_cart():
+        if len(driver.find_elements_by_css_selector('#box-checkout-summary tr')) > 2:
+            old_row = driver.find_elements_by_css_selector('#box-checkout-summary tr')[1]
+            driver.find_element_by_css_selector('button[name = "remove_cart_item"]').click()
+            WebDriverWait(driver, 3).until(EC.staleness_of(old_row))
+        else:
+            driver.find_element_by_css_selector('button[name = "remove_cart_item"]').click()
+
+    def is_element_present(selector):
         try:
             driver.find_element_by_css_selector(selector)
         except NoSuchElementException:
             return False
         return True
 
+    def is_alert_present():
+        try:
+            WebDriverWait(driver, 5).until(EC.alert_is_present())
+            alert = driver.switch_to.alert
+            alert.accept()
+        except TimeoutException:
+            pass
+
+
     login_as_customer(driver)
     for i in range(3):
         add_item_to_cart()
-    driver.find_element_by_css_selector()
     driver.find_element_by_xpath(f"//*[contains(text(), 'Checkout')]").click()
+    WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, '#order_confirmation-wrapper')))
+    while is_element_present('#box-checkout-summary'):
+        del_item_from_cart()
+    else:
+        driver.get('http://localhost:8080/litecart/en/')
+        assert driver.find_element_by_css_selector("#cart-wrapper .quantity").get_attribute(
+            "textContent") == '0'
