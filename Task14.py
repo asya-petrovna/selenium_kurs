97399489a0e2805c62dd9d334a493a223ed4bec4
Asya Isaeva <asya.gush@gmail.com>
2018-11-20 20:34:14 +0100

    Testcase are new windows open

--- Task14.py
+++ Task14.py
@@ -1,10 +1,6 @@
 import pytest
 from selenium.webdriver.remote.webdriver import WebDriver
 from drivers import drivers
-from selenium.webdriver.common.keys import Keys
-import time
-from selenium.webdriver.support.ui import Select
-import os
 from steps import login_litecart
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -18,27 +14,21 @@ def driver(request):
 
 
 def test_check_link_in_new_window(driver: WebDriver):
-    def open_new_window(number_of_link):
+    def open_new_window(external_link):
         set_before = set(driver.window_handles)
-        driver.find_elements_by_css_selector("#content a")[number_of_link].click()
+        external_link.click()
         WebDriverWait(driver, 5).until(EC.new_window_is_opened)
         set_after = set(driver.window_handles)
         new_window = set_after - set_before
         assert len(new_window) == 1
         driver.switch_to.window(list(new_window)[0])
-        time.sleep(3)
         driver.close()
         driver.switch_to.window(main_window)
 
     login_litecart(driver)
     driver.find_elements_by_css_selector("#app-")[2].click()
     driver.find_element_by_css_selector('.button').click()
-    driver.find_element_by_css_selector("input[value = '1']").click()
     main_window = driver.current_window_handle
-    for i in (1, 2, 3, 5, 6, 7, 8):
-        open_new_window(i)
-    driver.find_elements_by_css_selector("#content a")[4].click()
-    WebDriverWait(driver, 5).until(EC.alert_is_present())
-    alert = driver.switch_to.alert
-    time.sleep(2)
-    alert.accept()
+    links = driver.find_elements_by_css_selector(".fa-external-link")
+    for link in links:
+        open_new_window(link)
