2ef3831f26429644f09080d284ad581c3c81a507
Asya Isaeva <asya.gush@gmail.com>
2019-05-11 14:10:09 +0200

    add wait

--- unitt.py
+++ unitt.py
@@ -4,6 +4,10 @@ import os
 import unittest
 from selenium import webdriver
 import time
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
 
 class TestsForTnfWidget(unittest.TestCase):
 
@@ -16,10 +20,8 @@ class TestsForTnfWidget(unittest.TestCase):
         chrome = self.chrome
         chrome.get("https://www.thenorthface.de/shop/de/tnf-de/damen-jacken/damen-fanorak-2-0-jacke-3sv8?variationId=L48")
         chrome.find_element_by_css_selector('span.close').click()
-        time.sleep(3)
-
+        WebDriverWait(chrome, 4).until(EC.presence_of_element_located((By.CSS_SELECTOR,'.fitanalytics__button-text')))
         chrome.find_element_by_css_selector('.fitanalytics__button-text').click()
-        # chrome.find_element_by_css_selector('#fitanalytics__button img').click()
         time.sleep(2)
 
 
