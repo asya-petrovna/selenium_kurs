5c9f5cf7b3dae050b8839e404d09f806d9fb82f5
Asya Isaeva <asya.gush@gmail.com>
2019-05-12 14:45:15 +0200

    Structure for test in Page Object style (preliminary next steps)

--- test_for_widget_with_steps.py
+++ test_for_widget_with_steps.py
@@ -1,46 +1,54 @@
+from unittest import TestCase
+
+from drivers import DriverFactory
 from steps_for_widget import UsedSteps
 
 
-class Tests_using_wodget_for_different_sites(unittest.TestCase, UsedSteps):
+class TestsUsingWidgetForDifferentSites(TestCase):
+
+    def setUp(self):
+        self.driver = DriverFactory().create_chrome_driver()
+        self.steps = UsedSteps(
+            self.driver)  # initializirovala stepi iz UsedSteps, teper' oni rabotaut c ukazannim driverom
 
     @parameterized.expand([
         'https://www.thenorthface.de/shop/de/tnf-de/damen-jacken/damen-fanorak-2-0-jacke-3sv8?variationId=L48'
     ])
-    def test_parameterized_for_classic_customization(self, PDP):
-        chrome = self.chrome
-        self.setUp()
-        chrome.get(PDP)
-        self.find_and_click_on_element('span.close')
+    def test_parameterized_for_classic_customization(self, pdp):
+        driver = self.driver
+        self.driver.get(pdp)
+        self.steps.find_and_click_on_element('span.close')
+        self.steps.wait_until_element_presence(By.CSS_SELECTOR, '.fitanalytics__button-text')
+        self.steps.find_and_click_on_element('.fitanalytics__button-text')
+        self.steps.wait_until_element_presence(By.XPATH, '//*[contains(.,"Ihre Angaben")]')
 
-        chrome.find_element_by_css_selector().click()
-        WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, '.fitanalytics__button-text')))
-        chrome.find_element_by_css_selector('.fitanalytics__button-text').click()
-        WebDriverWait(chrome, 4).until(EC.presence_of_element_located((By.XPATH, '//*[contains(.,"Ihre Angaben")]')))
-        chrome.find_element_by_css_selector("input#uclw_form_height").send_keys('165')
-        chrome.find_element_by_css_selector("input#uclw_form_weight").send_keys('60')
-        chrome.find_element_by_css_selector("button#uclw_save_info_button").click()
-        WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(.,"Ihre Bauchform")]')))
-        WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "li.uclw_item.uclw_key_1")))
+        driver.find_element_by_css_selector("input#uclw_form_height").send_keys('165')
+        driver.find_element_by_css_selector("input#uclw_form_weight").send_keys('60')
+        self.steps.find_and_click_on_element("button#uclw_save_info_button")
+
+        driver.find_element_by_css_selector().click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(.,"Ihre Bauchform")]')))
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "li.uclw_item.uclw_key_1")))
         time.sleep(1)
-        chrome.find_element_by_css_selector('li.uclw_item.uclw_key_1').click()
-        WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(.,"Ihre Hüftform")]')))
-        WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "li.uclw_item.uclw_key_1")))
+        driver.find_element_by_css_selector('li.uclw_item.uclw_key_1').click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(.,"Ihre Hüftform")]')))
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "li.uclw_item.uclw_key_1")))
         time.sleep(1)
-        chrome.find_element_by_css_selector('span#uclw_item_shape_1').click()
-        WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(.,"hinzufügen")]')))
+        driver.find_element_by_css_selector('span#uclw_item_shape_1').click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(.,"hinzufügen")]')))
         time.sleep(2)
-        chrome.find_element_by_css_selector('.uclw_section.uclw_left .uclw_item.uclw_key_2').click()
-        chrome.find_element_by_css_selector('.uclw_section.uclw_right .uclw_item.uclw_key_2').click()
-        chrome.find_element_by_css_selector('button.uclw_button.uclw_submit_form_button').click()
-        WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input.uclw_input_text')))
-        chrome.find_element_by_css_selector('input.uclw_input_text').send_keys('30')
-        chrome.find_element_by_css_selector('button.uclw_button.uclw_submit_form_button').click()
+        driver.find_element_by_css_selector('.uclw_section.uclw_left .uclw_item.uclw_key_2').click()
+        driver.find_element_by_css_selector('.uclw_section.uclw_right .uclw_item.uclw_key_2').click()
+        driver.find_element_by_css_selector('button.uclw_button.uclw_submit_form_button').click()
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'input.uclw_input_text')))
+        driver.find_element_by_css_selector('input.uclw_input_text').send_keys('30')
+        driver.find_element_by_css_selector('button.uclw_button.uclw_submit_form_button').click()
         time.sleep(2)
-        WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span.uclw_field_label')))
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'span.uclw_field_label')))
 
         Keys.TAB()
         Keys.ARROW_LEFT
         time.sleep(2)
-        WebDriverWait(chrome, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(.,"Etwas enger")]')))
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[contains(.,"Etwas enger")]')))
         time.sleep(2)
-        chrome.find_element_by_css_selector('button.uclw_button.uclw_submit_form_button').click()
+        driver.find_element_by_css_selector('button.uclw_button.uclw_submit_form_button').click()
